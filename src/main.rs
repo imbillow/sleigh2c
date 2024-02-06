f5b1797072f31e4b7592362fbaba1c262da8a77b
billow <billow.fun@gmail.com>
2024-02-06 17:50:27 +0800

    Sleigh2rust!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
+use sleigh2rust::generate_disassembler;
 use sleigh_rs::file_to_sleigh;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let path = "../ghidra/Ghidra/Processors/v850/data/languages/v850.slaspec";
     let sleigh = file_to_sleigh(path.as_ref())?;
+    // generate_disassembler(path)?;
     println!("{:?}", sleigh);
     Ok(())
 }
